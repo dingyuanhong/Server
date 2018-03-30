0e0e9a871c314401a5584075471d373d9cc96dee
yhding <ilooyo2014fz>
2018-03-30 14:57:14 +0800

    增加清理脚本

---
+++ win32/clear.bat
@@ -0,0 +1,11 @@
+rm -rf ./client/Debug
+rm -rf ./client/Release
+rm -rf ./client/x64
+
+rm -rf ./core/Debug
+rm -rf ./core/Release
+rm -rf ./core/x64
+
+rm -rf ./server/Debug
+rm -rf ./server/Release
+rm -rf ./server/x64
