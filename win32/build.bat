e53f52405df0db81049e9cb9d5cbc578535d67ed
dingyuanhong <754046513@qq.com>
2018-04-05 14:33:59 +0800

    提交win32编译脚本

---
+++ win32/build.bat
@@ -0,0 +1,17 @@
+@if '%VS120COMNTOOLS%' == '' (
+	set BUILD="%VS120COMNTOOLS%/../IDE/devenv.exe"
+) else (
+	set BUILD="%VS140COMNTOOLS%/../IDE/devenv.exe"
+)
+
+@set BUILD="%TOOLS%/devenv.exe"
+
+@if '%VS120COMNTOOLS%' == '' (
+	%BUILD% server.sln /Upgrade
+	rmdir /s /q Backup
+	del /q UpgradeLog*.htm
+)
+
+@%BUILD% server.sln /build "Debug|Win32" /project core
+@%BUILD% server.sln /build "Debug|Win32" /project client
+@%BUILD% server.sln /build "Debug|Win32" /project server
